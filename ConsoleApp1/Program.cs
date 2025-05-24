2d2fb03ea9ac97cc52fc9be60b24808e47778d33
ForgeDom <maxstill2006@gmail.com>
2025-05-24 15:10:26 +0300

    +

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace SystemProgramming6;
+
+class Program
+{
+    static Semaphore semaphore = new Semaphore(3, 3);
+
+    static void Main()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Thread thread = new Thread(ThreadWork);
+            thread.Start(i + 1);
+        }
+
+        Console.ReadKey();
+    }
+
+    static void ThreadWork(object threadNumber)
+    {
+        semaphore.WaitOne();
+
+        try
+        {
+            Console.WriteLine($"Потік #{threadNumber} (ID: {Thread.CurrentThread.ManagedThreadId}) почав роботу.");
+
+            Random rnd = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Потік #{threadNumber}: {rnd.Next(1, 100)}");
+                Thread.Sleep(200);
+
+                Console.WriteLine($"Потік #{threadNumber} завершив роботу.");
+            }
+        }
+        finally
+        {
+            semaphore.Release();
+        }
+    }
+}
